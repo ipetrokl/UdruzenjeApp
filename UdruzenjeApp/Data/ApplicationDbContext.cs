d92bffeeab949f30b251c4011bf8a1a43e25d1f7
Nedžad Lipovac <nedzad.lipovac@edu.fit.ba>
2019-06-11 18:23:55 +0200

    plesk promjene

--- UdruzenjeApp/Data/ApplicationDbContext.cs
+++ UdruzenjeApp/Data/ApplicationDbContext.cs
@@ -21,10 +21,10 @@ namespace UdruzenjeApp.Data
         public DbSet<PredlozeniDogadjaj> predlozeniDogadjaj { get; set; }
 
 
-
+        //User ID = UdruzenjeApp1; Password=Pa$$word1;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        { optionsBuilder.UseSqlServer("Server=.;Database=UdruzenjeApp;Trusted_Connection=True;MultipleActiveResultSets=true;"); }
+        { optionsBuilder.UseSqlServer("Server=app.fit.ba,1433;Database=UdruzenjeApp;Trusted_Connection=false;User ID=UdruzenjeApp;Password=Pa$$word1;MultipleActiveResultSets=true"); }
 
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
